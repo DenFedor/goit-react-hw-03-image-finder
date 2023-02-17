59c94a1a284b7c689107c67b1d78062681be952f
DenFedor <fedorenko.wo@gmail.com>
2023-02-17 17:11:55 +0200

    Scroll with open modal
    Scroll disabled while modal is open

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,9 +6,11 @@ const modalRoot = document.querySelector('#modal-root');
 export default class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow='hidden';
   }
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow='unset';
   }
   handleKeyDown = e => {
     if (e.code === 'Escape') {
